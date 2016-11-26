560056055229c623a7539be4ed9f117db6574d4c
Timur Taipov <taipov@gmail.com>
2016-11-26 23:50:32 +0500

    few refactoring in sensors module

--- app/router.js
+++ app/router.js
@@ -9,8 +9,11 @@ Router.map(function() {
   this.route('dashboard');
   this.route('fourOhFour', { path: "*path"});
   this.route('system');
-  this.route('sensors');
-  this.route('sensor', {path: "/sensors/:sensor_id"});
+  this.route('sensors',  function() {
+    this.route('add');
+    this.route('show', {path: "/:sensor_id"});
+    this.route('delete', {path: "/delete/:sensor_id"});
+  });
 });
 
 export default Router;
--- app/routes/sensors/index.js
+++ app/routes/sensors/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.store.findRecord('sensor', params.sensor_id);
+        return this.get('store').findAll('sensor');
     }
 });
